9015aee72ee36fb58274f98e368f1bc4c55ccc6c
Moshlehuddin.Mazumder <moshlehuddin.mazumder@paymenttools.com>
2025-05-04 20:06:43 +0200

    update build-image sh file

--- build-image.sh
+++ build-image.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 # This script builds the Docker image for the project.
-version=0.0.2
-./gradlew bootBuildImage \
-  --imageName=apu84/inventory:$version \
-  --builder=paketobuildpacks/builder:base \
-  -PBP_PLATFORM=linux/amd64
+version=0.0.1
+./gradlew bootBuildImage --imageName=apu84/inventory:$version
 podman --connection podman-machine-default-root push docker.io/apu84/inventory:$version
